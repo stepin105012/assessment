0a2d944ccd92024ca00390c9def940ef07d7c14e
Binitta <69056129+stepin105012@users.noreply.github.com>
2020-09-23 22:56:26 -0700

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,38 @@
+[
+    {
+        "name": "Sweet and spicy",
+        "city": "Mysore",
+        "type": "Veg",
+        "cuisine": ["Indian", "Italian"]
+    },
+    {
+        "name": "Surabhi",
+        "city": "TVM",
+        "type": "Veg",
+        "cuisine": ["Indian"]
+    },
+    {
+        "name": "jova Hotel",
+        "city": "Banglore",
+        "type": "Non-Veg",
+        "cuisine": ["Indian", "Chinese", "Arabian"]
+    },
+    {
+        "name": "Paragon",
+        "city": "Kochi",
+        "type": "Non-Veg",
+        "cuisine": ["Italian", "Chinese"]
+    },
+    {
+        "name": "Mothers plaza",
+        "city": "Kollam",
+        "type": "Non-Veg",
+        "cuisine": ["Arabian", "Chinese"]
+    },
+    {
+        "name": "Akshaya",
+        "city": "Mysore",
+        "type": "Veg",
+        "cuisine": ["Indian"]
+    }
+]
